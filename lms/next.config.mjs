f16592d15c6d5852373e395319a7691b0484df44
shikhar746 <shikhar.srivastava746@gmail.com>
2026-08-08 13:48:51 +0530

    revert: reset next.config.mjs back to default config as requested

--- lms/next.config.mjs
+++ lms/next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      }
-    }
-    return config
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
